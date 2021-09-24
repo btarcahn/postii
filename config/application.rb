c16a430fa441f09569961699419d72c2f44d0be9
btarcahn <bach.tran@employmenthero.com>
2021-09-24 14:53:13 +0700

    Accept linux environment to build w Heroku

--- config/application.rb
+++ config/application.rb
@@ -36,6 +36,7 @@ module Postii
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
-    Dotenv::Railtie.load
+
+    Dotenv::Railtie.load if Rails.env == 'development'
   end
 end
