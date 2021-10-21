23eaed15cbc941cff3aa576a687e01ddfa295812
btarcahn <bach.tran@employmenthero.com>
2021-10-21 14:53:28 +0700

    Change API for Alerts and add rspec tests

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::API
   include ActionController::StrongParameters
   include ActionController::MimeResponds
-  include CommonHelper
+  include AlertHelpers
 
 end
